d9a636c7c91996203737698a4badfaa1243b159d
Mike Branstein <mike.branstein@gmail.com>
2016-09-21 17:07:42 -0400

    refactor to persist data

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -10,22 +10,32 @@ export class HeroService {
     constructor(private http: Http) {}
 
     private heroesUrl = 'app/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     getHeroes() : Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
             .toPromise()
             .then(response => response.json().data as Hero[])
             .catch(this.handleError);
-    };
+    }
 
     getHero(id : number) : Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
-    };
+    }
+
+    update(hero: Hero) {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
 
     private handleError(error: any): Promise<any> {
-      console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 
 }
